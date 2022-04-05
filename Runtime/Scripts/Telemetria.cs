f260749c951e90a9b0a9df36803aeac5b7d5cba4
David Godoy <dagodoy@ucm.es>
2022-04-05 20:28:12 +0200

    Inicio del package

---
+++ Runtime/Scripts/Telemetria.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Grupo06
+{
+    public class Telemetria : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public int a()
+        {
+            return 1;
+        }
+    }
+}
+
