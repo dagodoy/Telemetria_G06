c2d5fc24347e196a7f582bcab396eebe6ff76b32
David Godoy <dagodoy@ucm.es>
2022-04-05 21:19:05 +0200

    Funciona

--- Runtime/Scripts/Telemetria.cs
+++ Runtime/Scripts/Telemetria.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Telemetria : MonoBehaviour
+namespace Grupo06
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Telemetria : MonoBehaviour
     {
-        
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        }
 
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public static int a() { return 1; }
+    }
 }
+
