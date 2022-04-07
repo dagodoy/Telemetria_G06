055c10b3ac35ff19c2aa67d73b84a4e6e3529279
dagodoy <dagodoy@ucm.es>
2022-04-07 17:39:41 +0200

    Eventos pero en el mismo archivo + fluent

---
+++ Runtime/Scripts/Eventos.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//namespace Grupo06
+//{
+//    //public abstract class Event {
+//    //    public float tiempo;
+//    //    public int sesion;
+
+//    //    public Event Sesion(int s)
+//    //    {
+//    //        sesion = s;
+//    //        return this;
+//    //    }
+
+//    //    public Event Tiempo(float t)
+//    //    {
+//    //        tiempo = t;
+//    //        return this;
+//    //    }
+//    //}
+
+//    //public class LevelStartEvent : Event 
+//    //{
+//    //    public int nivel;
+
+//    //    public LevelStartEvent Nivel(int n)
+//    //    {
+//    //        nivel = n;
+//    //        return this;
+//    //    }
+//    //}
+
+//    //public class LevelEndEvent : Event {
+//    //    public int nivel;
+
+//    //    public LevelEndEvent Nivel(int n)
+//    //    {
+//    //        nivel = n;
+//    //        return this;
+//    //    }
+//    //}
+
+//    //public class PauseEvent : Event { }
+
+//    //public class DeathEvent : Event
+//    //{
+//    //    public float x;
+//    //    public float y;
+
+//    //    public DeathEvent X(float X)
+//    //    {
+//    //        x = X;
+//    //        return this;
+//    //    }
+
+//    //    public DeathEvent Y(float Y)
+//    //    {
+//    //        y = Y;
+//    //        return this;
+//    //    }
+//    //}
+
+//    //public class JumpEvent : Event
+//    //{
+//    //    public float x;
+//    //    public float y;
+
+//    //    public JumpEvent X(float X)
+//    //    {
+//    //        x = X;
+//    //        return this;
+//    //    }
+
+//    //    public JumpEvent Y(float Y)
+//    //    {
+//    //        y = Y;
+//    //        return this;
+//    //    }
+//    //}
+
+//    //public class CollisionEvent : Event
+//    //{
+//    //    public int id;
+//    //    public string tag;
+
+//    //    public CollisionEvent Id(int identificador)
+//    //    {
+//    //        id = identificador;
+//    //        return this;
+//    //    }
+
+//    //    public CollisionEvent Id(string etiqueta)
+//    //    {
+//    //        tag = etiqueta;
+//    //        return this;
+//    //    }
+//    //}
+//}
+
--- Runtime/Scripts/Telemetria.cs
+++ Runtime/Scripts/Telemetria.cs
@@ -6,19 +6,139 @@ namespace Grupo06
 {
     public class Telemetria : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        private static Telemetria instance = null;
 
+        private Telemetria() { }
+
+        public static Telemetria Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Telemetria();
+                }
+                return instance;
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        public LevelStartEvent LevelStart()
         {
+            return new LevelStartEvent();
+        }
+        public LevelEndEvent LevelEnd()
+        {
+            return new LevelEndEvent();
+        }
+        public PauseEvent Pause()
+        {
+            return new PauseEvent();
+        }
+        public DeathEvent Death()
+        {
+            return new DeathEvent();
+        }
+        public JumpEvent Jump()
+        {
+            return new JumpEvent();
+        }
+        public CollisionEvent Collision()
+        {
+            return new CollisionEvent();
+        }
+    }
 
+    /// <summary>
+    /// DOLOR
+    /// </summary>
+
+    public abstract class Event
+    {
+        public float tiempo { get; set; }
+        public int sesion { get; set; }
+
+        public Event()
+        {
+            tiempo = Time.realtimeSinceStartup;
+            sesion = 3;
+        }
+    }
+
+    public class LevelStartEvent : Event
+    {
+        public int nivel;
+
+        public LevelStartEvent Nivel(int n)
+        {
+            nivel = n;
+            return this;
+        }
+    }
+
+    public class LevelEndEvent : Event
+    {
+        public int nivel;
+
+        public LevelEndEvent Nivel(int n)
+        {
+            nivel = n;
+            return this;
+        }
+    }
+
+    public class PauseEvent : Event { }
+
+    public class DeathEvent : Event
+    {
+        public float x;
+        public float y;
+
+        public DeathEvent X(float X)
+        {
+            x = X;
+            return this;
         }
 
-        public static int a() { return 1; }
+        public DeathEvent Y(float Y)
+        {
+            y = Y;
+            return this;
+        }
+    }
+
+    public class JumpEvent : Event
+    {
+        public float x;
+        public float y;
+
+        public JumpEvent X(float X)
+        {
+            x = X;
+            return this;
+        }
+
+        public JumpEvent Y(float Y)
+        {
+            y = Y;
+            return this;
+        }
+    }
+
+    public class CollisionEvent : Event
+    {
+        public int id;
+        public string tag;
+
+        public CollisionEvent Id(int identificador)
+        {
+            id = identificador;
+            return this;
+        }
+
+        public CollisionEvent Id(string etiqueta)
+        {
+            tag = etiqueta;
+            return this;
+        }
     }
 }
-
