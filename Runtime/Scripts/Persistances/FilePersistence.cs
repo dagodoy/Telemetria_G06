f90e98194c44c1f4c32ddd456783c1b6de911117
David Carmona <davicarm@ucm.es>
2022-06-07 11:37:09 +0200

    persistencia acutalizada

--- Runtime/Scripts/Persistances/FilePersistence.cs
+++ Runtime/Scripts/Persistances/FilePersistence.cs
@@ -5,30 +5,47 @@ using System.IO;
 
 namespace Grupo06
 {
+
     public class FilePersistence : Persistence
     {
-        public FilePersistence(Serializer s) : base(s) { }
+        public FilePersistence(Serializer s) : base(s)
+        {
+            CreateDirectory();
+        }
+        void CreateDirectory()
+        {
+            directory = "./Resultado del test";
+
+            if (Directory.Exists(directory))
+            {
+                string[] files = Directory.GetFiles(directory);
+                foreach (string file in files)
+                {
+                    File.Delete(file);
+                }
+                Directory.Delete(directory);
+
+            }
+            Directory.CreateDirectory(directory);
+        }
         public override void Send(Event e)
         {
             string s = serializer.Serialize(e);
             //Aqui tiene que guardar el string en un archivo
             //string directory = Application.persistentDataPath + "/Telemetria_G06" + "/HW_" + SystemInfo.deviceUniqueIdentifier; //directory
-            //DIRECTORIO A PELO CAMBIARLO PARA QUE ESTE BIEN
-            string directory = "../Pruebas" + "/Telemetria_G06" + "/HW_" + SystemInfo.deviceUniqueIdentifier;
-            string name = "/ID_" + e.sesion + serializer.getExtension(); //name
 
-            if (!Directory.Exists(directory))
-                Directory.CreateDirectory(directory);
-
+            string fileName = "/ID_" + e.sesion + serializer.getExtension(); //name
             FileStream fs;
-            if (!File.Exists(directory + name))
-                fs = File.Open(directory + name, FileMode.Create);
+            if (!File.Exists(directory + fileName))
+                fs = File.Open(directory + fileName, FileMode.Create);
             else
-                fs = File.Open(directory + name, FileMode.Append);
+                fs = File.Open(directory + fileName, FileMode.Append);
 
             StreamWriter writer = new StreamWriter(fs);
             writer.WriteLine(s);
             writer.Close();
         }
+
+        string directory;
     }
 }
